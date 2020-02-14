e5511e4a9651d36e6083b4b8214bc2fd95c8f883
Julian Saavedra (LCL-C) <Julian.Saavedra@loblaw.ca>
2020-02-14 10:23:00 -0500

    first commit

---
+++ src/test/java/com/jfsaaved/jenkins/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfsaaved.jenkins.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
