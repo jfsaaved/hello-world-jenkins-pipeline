f812599952abdc94e0d2fe886215dc26400cc2e2
Julian Saavedra (LCL-C) <Julian.Saavedra@loblaw.ca>
2020-02-18 14:53:59 -0500

    agent any

--- src/main/java/com/jfsaaved/jenkins/pipeline/components/ImagePrescriptionDecoder.java
+++ src/main/java/com/jfsaaved/jenkins/pipeline/components/ImagePrescriptionDecoder.java
@@ -1,12 +1,8 @@
 package com.jfsaaved.jenkins.pipeline.components;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jfsaaved.jenkins.pipeline.models.Image;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.Base64;
 
 @Component
 public class ImagePrescriptionDecoder {
